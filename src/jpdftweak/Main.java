19d5c1af4e7ce2a58b2775908932f5ab26dfce32
xlance-github <74504527+xlance-github@users.noreply.github.com>
2021-03-17 12:56:45 -0400

    Update Main.java
    Updated String VERSION from XIV  to  XV

--- src/jpdftweak/Main.java
+++ src/jpdftweak/Main.java
@@ -14,7 +14,7 @@ import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 
 public class Main {
-	public static final String VERSION = "1.1 Stage XIV";
+	public static final String VERSION = "1.1 Stage XV";
 
 	public static void main(final String[] args) {
 			
