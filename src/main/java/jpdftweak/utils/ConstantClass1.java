2193800d5a51de8f6edc1c7d5363e1bc0e21262c
Vasilis Naskos <vnaskos@gmail.com>
2022-12-13 18:02:54 +0100

    Cleanup unused files

--- src/main/java/jpdftweak/utils/ConstantClass1.java
+++ src/main/java/jpdftweak/utils/ConstantClass1.java
@@ -2,13 +2,9 @@ package jpdftweak.utils;
 
 public class ConstantClass1 {
 	private static Integer id;
-	private static Integer preId;
-	private static Integer fid;
 
 	static {
 		ConstantClass1.id = 0;
-		ConstantClass1.preId = 0;
-		ConstantClass1.fid = 0;
 	}
 
 	public Integer getId() {
@@ -19,24 +15,6 @@ public class ConstantClass1 {
 		ConstantClass1.id = id;
 	}
 
-	public Integer getPreId() {
-		return ConstantClass1.preId;
-	}
-
-	public void setPreId(final Integer preId) {
-		ConstantClass1.preId = preId;
-	}
-
-	public Integer getFid() {
-		return ConstantClass1.fid;
-	}
-
-	public void setFid(final Integer fid) {
-		ConstantClass1.fid = fid;
-	}
-
-//	*** Controls "ID" Numbering ***	
-	
 	public String getStringAsId() {
 		String str = "";
 		++ConstantClass1.id;
@@ -44,22 +22,5 @@ public class ConstantClass1 {
 			str = str.concat("0");
 		}
 		return str.concat(ConstantClass1.id.toString());
-	}
-
-	public String getStringAsPreId() {
-		String str = "";
-		++ConstantClass1.preId;
-		for (int i = 10; i > ConstantClass1.preId.toString().length(); --i) {
-			str = str.concat("0");
-		}
-		return str.concat(ConstantClass1.preId.toString());
-	}
-
-	public String getIdString(final Integer idString) {
-		String str = "";
-		for (int i = 10; i > idString.toString().length(); --i) {
-			str = str.concat("0");
-		}
-		return str.concat(idString.toString());
 	}
 }
