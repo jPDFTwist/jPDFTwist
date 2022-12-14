dc6c4d2f5bbbba1a4823618392946ef89d8739df
Vasilis Naskos <vnaskos@gmail.com>
2022-12-14 20:24:21 +0100

    GUI: Move FileChooser to gui components

--- src/main/java/jpdftwist/tabs/BookmarkTab.java
+++ src/main/java/jpdftwist/tabs/BookmarkTab.java
@@ -8,9 +8,9 @@ import com.jgoodies.forms.layout.FormLayout;
 import jpdftwist.core.PDFTwist;
 import jpdftwist.core.PdfBookmark;
 import jpdftwist.gui.MainWindow;
+import jpdftwist.gui.component.FileChooser;
 import jpdftwist.gui.component.table.TableComponent;
 import jpdftwist.gui.dialog.OutputProgressDialog;
-import jpdftwist.tabs.input.FileChooser;
 import jpdftwist.utils.PdfParser;
 
 import javax.swing.*;
@@ -30,173 +30,173 @@ import java.util.List;
 
 public class BookmarkTab extends Tab {
 
-	private JButton load;
-	private JButton importPDF, importCSV, exportCSV;
-	private TableComponent bookmarks;
-	private JCheckBox changeBookmarks;
-	private final MainWindow mainWindow;
+    private JButton load;
+    private JButton importPDF, importCSV, exportCSV;
+    private TableComponent bookmarks;
+    private JCheckBox changeBookmarks;
+    private final MainWindow mainWindow;
 
-	public BookmarkTab(MainWindow mf) {
-		super(new FormLayout("f:p:g, f:p", "f:p, f:p, f:p:g"));
-		this.mainWindow = mf;
-		CellConstraints CC = new CellConstraints();
-		add(changeBookmarks = new JCheckBox("Change chapter bookmarks"), CC.xy(1, 1));
-		changeBookmarks.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				updateEnabledState();
-			}
-		});
-		add(load = new JButton("Load from document"), CC.xy(2, 1));
-		load.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				List<PdfBookmark> bm = mainWindow.getInputTab().loadBookmarks();
-				bookmarks.clear();
-				appendBookmarks(bm);
-			}
-		});
-		JPanel panel = new JPanel(new GridLayout(1, 3));
-		panel.add(importPDF = new JButton("Import from PDF"));
-		importPDF.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				FileChooser fileChooser = new FileChooser();
+    public BookmarkTab(MainWindow mf) {
+        super(new FormLayout("f:p:g, f:p", "f:p, f:p, f:p:g"));
+        this.mainWindow = mf;
+        CellConstraints CC = new CellConstraints();
+        add(changeBookmarks = new JCheckBox("Change chapter bookmarks"), CC.xy(1, 1));
+        changeBookmarks.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                updateEnabledState();
+            }
+        });
+        add(load = new JButton("Load from document"), CC.xy(2, 1));
+        load.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                List<PdfBookmark> bm = mainWindow.getInputTab().loadBookmarks();
+                bookmarks.clear();
+                appendBookmarks(bm);
+            }
+        });
+        JPanel panel = new JPanel(new GridLayout(1, 3));
+        panel.add(importPDF = new JButton("Import from PDF"));
+        importPDF.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                FileChooser fileChooser = new FileChooser();
 
-				JFileChooser chooser = fileChooser.getFileChooser();
-				if (chooser.showOpenDialog(mainWindow) == JFileChooser.APPROVE_OPTION) {
-					try {
-						bookmarks.clear();
-						PdfReader reader = PdfParser.open(chooser.getSelectedFile().getAbsolutePath(), false);
-						List bmk = SimpleBookmark.getBookmark(reader);
-						appendBookmarks(PdfBookmark.parseBookmarks(bmk, 1));
-						reader.close();
-					} catch (Exception ex) {
-						ex.printStackTrace();
-						JOptionPane.showMessageDialog(mainWindow, ex.getMessage(), "Error reading file",
-								JOptionPane.ERROR_MESSAGE);
-					}
-				}
-			}
-		});
-		panel.add(importCSV = new JButton("Import from CSV"));
-		importCSV.addActionListener(new ActionListener() {
+                JFileChooser chooser = fileChooser.getFileChooser();
+                if (chooser.showOpenDialog(mainWindow) == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        bookmarks.clear();
+                        PdfReader reader = PdfParser.open(chooser.getSelectedFile().getAbsolutePath(), false);
+                        List bmk = SimpleBookmark.getBookmark(reader);
+                        appendBookmarks(PdfBookmark.parseBookmarks(bmk, 1));
+                        reader.close();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                        JOptionPane.showMessageDialog(mainWindow, ex.getMessage(), "Error reading file",
+                            JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            }
+        });
+        panel.add(importCSV = new JButton("Import from CSV"));
+        importCSV.addActionListener(new ActionListener() {
 
-			public void actionPerformed(ActionEvent e) {
-				JFileChooser jfc = new JFileChooser();
-				if (jfc.showOpenDialog(mainWindow) == JFileChooser.APPROVE_OPTION) {
-					importCSV(jfc.getSelectedFile());
-				}
-			}
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser jfc = new JFileChooser();
+                if (jfc.showOpenDialog(mainWindow) == JFileChooser.APPROVE_OPTION) {
+                    importCSV(jfc.getSelectedFile());
+                }
+            }
 
-		});
-		panel.add(exportCSV = new JButton("Export to CSV"));
-		exportCSV.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JFileChooser jfc = new JFileChooser();
-				if (jfc.showSaveDialog(mainWindow) == JFileChooser.APPROVE_OPTION) {
-					File f = jfc.getSelectedFile();
-					if (f.exists()) {
-						if (JOptionPane.showConfirmDialog(mainWindow, "Overwrite existing file?", "Confirm Overwrite",
-								JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.NO_OPTION) {
-							return;
-						}
-					}
-					exportCSV(f);
-				}
-			}
-		});
-		add(panel, CC.xyw(1, 2, 2));
-		add(bookmarks = new TableComponent(
-				new String[] { "Depth", "Open", "Title", "Page", "Position", "Bold", "Italic", "Options" },
-				new Class[] { Integer.class, Boolean.class, String.class, Integer.class, String.class, Boolean.class,
-						Boolean.class, String.class },
-				new Object[] { 1, false, "", 1, "", false, false, "" }), CC.xyw(1, 3, 2));
-		updateEnabledState();
-	}
+        });
+        panel.add(exportCSV = new JButton("Export to CSV"));
+        exportCSV.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser jfc = new JFileChooser();
+                if (jfc.showSaveDialog(mainWindow) == JFileChooser.APPROVE_OPTION) {
+                    File f = jfc.getSelectedFile();
+                    if (f.exists()) {
+                        if (JOptionPane.showConfirmDialog(mainWindow, "Overwrite existing file?", "Confirm Overwrite",
+                            JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.NO_OPTION) {
+                            return;
+                        }
+                    }
+                    exportCSV(f);
+                }
+            }
+        });
+        add(panel, CC.xyw(1, 2, 2));
+        add(bookmarks = new TableComponent(
+            new String[]{"Depth", "Open", "Title", "Page", "Position", "Bold", "Italic", "Options"},
+            new Class[]{Integer.class, Boolean.class, String.class, Integer.class, String.class, Boolean.class,
+                Boolean.class, String.class},
+            new Object[]{1, false, "", 1, "", false, false, ""}), CC.xyw(1, 3, 2));
+        updateEnabledState();
+    }
 
-	protected void importCSV(File selectedFile) {
-		try {
-			BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(selectedFile), "UTF-8"));
-			String line;
-			List<PdfBookmark> bmks = new ArrayList<PdfBookmark>();
-			while ((line = r.readLine()) != null) {
-				if (line.startsWith("#")) {
-					continue;
-				}
-				bmks.add(PdfBookmark.parseBookmark(line));
-			}
-			r.close();
-			bookmarks.clear();
-			appendBookmarks(bmks);
-		} catch (IOException ex) {
-			ex.printStackTrace();
-			JOptionPane.showMessageDialog(mainWindow, ex.getMessage(), "Error reading file", JOptionPane.ERROR_MESSAGE);
-		}
-	}
+    protected void importCSV(File selectedFile) {
+        try {
+            BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(selectedFile), "UTF-8"));
+            String line;
+            List<PdfBookmark> bmks = new ArrayList<PdfBookmark>();
+            while ((line = r.readLine()) != null) {
+                if (line.startsWith("#")) {
+                    continue;
+                }
+                bmks.add(PdfBookmark.parseBookmark(line));
+            }
+            r.close();
+            bookmarks.clear();
+            appendBookmarks(bmks);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(mainWindow, ex.getMessage(), "Error reading file", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 
-	protected void exportCSV(File selectedFile) {
-		try {
-			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(selectedFile), "UTF-8"));
-			for (int i = 0; i < bookmarks.getRowCount(); i++) {
-				PdfBookmark b = getBookmark(bookmarks.getRow(i));
-				w.write(b.toString());
-				w.newLine();
-			}
-			w.close();
-		} catch (IOException ex) {
-			ex.printStackTrace();
-			JOptionPane.showMessageDialog(mainWindow, ex.getMessage(), "Error reading file", JOptionPane.ERROR_MESSAGE);
-		}
-	}
+    protected void exportCSV(File selectedFile) {
+        try {
+            BufferedWriter w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(selectedFile), "UTF-8"));
+            for (int i = 0; i < bookmarks.getRowCount(); i++) {
+                PdfBookmark b = getBookmark(bookmarks.getRow(i));
+                w.write(b.toString());
+                w.newLine();
+            }
+            w.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(mainWindow, ex.getMessage(), "Error reading file", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 
-	protected void appendBookmarks(List<PdfBookmark> bm) {
-		for (PdfBookmark b : bm) {
-			bookmarks.addRow(b.getDepth(), b.isOpen(), b.getTitle(), b.getPage(), b.getPagePosition(), b.isBold(),
-					b.isItalic(), b.getMoreOptions());
-		}
-	}
+    protected void appendBookmarks(List<PdfBookmark> bm) {
+        for (PdfBookmark b : bm) {
+            bookmarks.addRow(b.getDepth(), b.isOpen(), b.getTitle(), b.getPage(), b.getPagePosition(), b.isBold(),
+                b.isItalic(), b.getMoreOptions());
+        }
+    }
 
-	protected PdfBookmark getBookmark(Object[] row) {
-		int depth = row[0] == null ? 1 : (Integer) row[0];
-		boolean open = (Boolean) row[1];
-		String title = (String) row[2];
-		int page = row[3] == null ? 1 : (Integer) row[3];
-		String pagePosition = (String) row[4];
-		boolean bold = (Boolean) row[5];
-		boolean italic = (Boolean) row[6];
-		String moreOptions = (String) row[7];
-		return new PdfBookmark(depth, title, open, page, pagePosition, bold, italic, moreOptions);
-	}
+    protected PdfBookmark getBookmark(Object[] row) {
+        int depth = row[0] == null ? 1 : (Integer) row[0];
+        boolean open = (Boolean) row[1];
+        String title = (String) row[2];
+        int page = row[3] == null ? 1 : (Integer) row[3];
+        String pagePosition = (String) row[4];
+        boolean bold = (Boolean) row[5];
+        boolean italic = (Boolean) row[6];
+        String moreOptions = (String) row[7];
+        return new PdfBookmark(depth, title, open, page, pagePosition, bold, italic, moreOptions);
+    }
 
-	protected void updateEnabledState() {
-		load.setEnabled(changeBookmarks.isSelected());
-		importPDF.setEnabled(changeBookmarks.isSelected());
-		importCSV.setEnabled(changeBookmarks.isSelected());
-		exportCSV.setEnabled(changeBookmarks.isSelected());
-		bookmarks.setEnabled(changeBookmarks.isSelected());
-	}
+    protected void updateEnabledState() {
+        load.setEnabled(changeBookmarks.isSelected());
+        importPDF.setEnabled(changeBookmarks.isSelected());
+        importCSV.setEnabled(changeBookmarks.isSelected());
+        exportCSV.setEnabled(changeBookmarks.isSelected());
+        bookmarks.setEnabled(changeBookmarks.isSelected());
+    }
 
-	
-	public String getTabName() {
-		return "Bookmarks";
-	}
 
-	
-	public void checkRun() throws IOException {
-		bookmarks.checkRun("chapter bookmarks");
-	}
+    public String getTabName() {
+        return "Bookmarks";
+    }
 
-	
-	public PDFTwist run(PDFTwist pdfTwist, OutputProgressDialog outDialog) throws IOException, DocumentException {
-		outDialog.updateJPDFTwistProgress(getTabName());
-		outDialog.setAction("Updating bookmarks");
-		outDialog.resetProcessedPages();
-		if (changeBookmarks.isSelected()) {
-			PdfBookmark[] bm = new PdfBookmark[bookmarks.getRowCount()];
-			for (int i = 0; i < bm.length; i++) {
-				bm[i] = getBookmark(bookmarks.getRow(i));
-			}
-			pdfTwist.updateBookmarks(bm);
-		}
-		return pdfTwist;
-	}
+
+    public void checkRun() throws IOException {
+        bookmarks.checkRun("chapter bookmarks");
+    }
+
+
+    public PDFTwist run(PDFTwist pdfTwist, OutputProgressDialog outDialog) throws IOException, DocumentException {
+        outDialog.updateJPDFTwistProgress(getTabName());
+        outDialog.setAction("Updating bookmarks");
+        outDialog.resetProcessedPages();
+        if (changeBookmarks.isSelected()) {
+            PdfBookmark[] bm = new PdfBookmark[bookmarks.getRowCount()];
+            for (int i = 0; i < bm.length; i++) {
+                bm[i] = getBookmark(bookmarks.getRow(i));
+            }
+            pdfTwist.updateBookmarks(bm);
+        }
+        return pdfTwist;
+    }
 
 }
--- src/main/java/jpdftwist/tabs/input/FileImporter.java
+++ src/main/java/jpdftwist/tabs/input/FileImporter.java
@@ -1,6 +1,7 @@
 package jpdftwist.tabs.input;
 
 import jpdftwist.core.FilenameUtils;
+import jpdftwist.gui.component.FileChooser;
 import jpdftwist.gui.component.treetable.Node;
 import jpdftwist.gui.component.treetable.TreeTableRowType;
 import jpdftwist.gui.component.treetable.event.PageEventListener;
@@ -21,194 +22,193 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
  * @author Vasilis Naskos
  */
 public class FileImporter implements Runnable {
 
-	private JFrame parentFrame;
+    private JFrame parentFrame;
 
-	private final InputProgressDialog importDialog;
-	private final ModelHandler modelHandler;
-	private final ErrorHandler errorHandler;
+    private final InputProgressDialog importDialog;
+    private final ModelHandler modelHandler;
+    private final ErrorHandler errorHandler;
 
-	private boolean useTempFiles, optimizePDF, autoRestrictionsOverwrite, autoRestrioctionsNew;
+    private boolean useTempFiles, optimizePDF, autoRestrictionsOverwrite, autoRestrioctionsNew;
 
-	private List<File[]> files;
-	private ProcessCancelledListener cancelListener;
+    private List<File[]> files;
+    private ProcessCancelledListener cancelListener;
 
-	private boolean cancel;
+    private boolean cancel;
 
-	public FileImporter(ModelHandler modelHandler) {
-		this.cancel = false;
-		this.useTempFiles = false;
-		this.optimizePDF = false;
-		this.autoRestrioctionsNew = true;
-		this.autoRestrictionsOverwrite = true;
+    public FileImporter(ModelHandler modelHandler) {
+        this.cancel = false;
+        this.useTempFiles = false;
+        this.optimizePDF = false;
+        this.autoRestrioctionsNew = true;
+        this.autoRestrictionsOverwrite = true;
 
-		this.errorHandler = new ErrorHandler();
-		this.importDialog = new InputProgressDialog();
-		this.cancelListener = () -> cancel = true;
-		importDialog.setCancelledListener(cancelListener);
+        this.errorHandler = new ErrorHandler();
+        this.importDialog = new InputProgressDialog();
+        this.cancelListener = () -> cancel = true;
+        importDialog.setCancelledListener(cancelListener);
 
-		this.modelHandler = modelHandler;
-	}
+        this.modelHandler = modelHandler;
+    }
 
-	public void setParentFrame(JFrame parentFrame) {
-		this.parentFrame = parentFrame;
-	}
+    public void setParentFrame(JFrame parentFrame) {
+        this.parentFrame = parentFrame;
+    }
 
-	public void setOptimizePDF(boolean optimizePDF) {
-		this.optimizePDF = optimizePDF;
-	}
+    public void setOptimizePDF(boolean optimizePDF) {
+        this.optimizePDF = optimizePDF;
+    }
 
-	public void setAutoRestrictionsOverwrite(boolean autoRestrictionsOverwrite) {
-		this.autoRestrictionsOverwrite = autoRestrictionsOverwrite;
-	}
+    public void setAutoRestrictionsOverwrite(boolean autoRestrictionsOverwrite) {
+        this.autoRestrictionsOverwrite = autoRestrictionsOverwrite;
+    }
 
-	public void setAutoRestrioctionsNew(boolean autoRestrioctionsNew) {
-		this.autoRestrioctionsNew = autoRestrioctionsNew;
-	}
+    public void setAutoRestrioctionsNew(boolean autoRestrioctionsNew) {
+        this.autoRestrioctionsNew = autoRestrioctionsNew;
+    }
 
-	public void setUseTempFiles(boolean useTempFiles) {
-		this.useTempFiles = useTempFiles;
-	}
+    public void setUseTempFiles(boolean useTempFiles) {
+        this.useTempFiles = useTempFiles;
+    }
 
-	public FileImporter(ModelHandler handler, File... f) {
-		this(handler);
-		files = new ArrayList<>();
-		files.add(f);
-	}
+    public FileImporter(ModelHandler handler, File... f) {
+        this(handler);
+        files = new ArrayList<>();
+        files.add(f);
+    }
 
-	public FileImporter(ModelHandler handler, List<File[]> files) {
-		this(handler);
-		this.files = files;
-	}
-	
-	public void run() {
-		if (files != null && !files.isEmpty()) {
-			showProgressDialog();
-			setProgressBarLimits();
+    public FileImporter(ModelHandler handler, List<File[]> files) {
+        this(handler);
+        this.files = files;
+    }
 
-			for (File[] fileArray : files) {
-				if (!importDialog.isVisible()) {
-					break;
-				}
-				importDirectory(fileArray);
-			}
+    public void run() {
+        if (files != null && !files.isEmpty()) {
+            showProgressDialog();
+            setProgressBarLimits();
 
-			importDialog.closeDialogWithDelay();
-			errorHandler.showErrors();
+            for (File[] fileArray : files) {
+                if (!importDialog.isVisible()) {
+                    break;
+                }
+                importDirectory(fileArray);
+            }
 
-			modelHandler.updateTableUI();
-			return;
-		}
+            importDialog.closeDialogWithDelay();
+            errorHandler.showErrors();
 
-		FileChooser fileChooser = new FileChooser();
-		File[] selectedFiles = fileChooser.getSelectedFiles();
-		if (selectedFiles == null)
-			return;
+            modelHandler.updateTableUI();
+            return;
+        }
 
-		showProgressDialog();
+        FileChooser fileChooser = new FileChooser();
+        File[] selectedFiles = fileChooser.getSelectedFiles();
+        if (selectedFiles == null)
+            return;
 
-		DirectoryScanner scanner = new DirectoryScanner(selectedFiles);
-		files = scanner.getFiles();
+        showProgressDialog();
 
-		setProgressBarLimits();
+        DirectoryScanner scanner = new DirectoryScanner(selectedFiles);
+        files = scanner.getFiles();
 
-		Thread t = new Thread(() -> {
-			for (File[] directory : files) {
-				if (cancel) {
-					break;
-				}
-				importDirectory(directory);
-			}
+        setProgressBarLimits();
 
-			importDialog.closeDialogWithDelay();
-			errorHandler.showErrors();
-			modelHandler.updateTableUI();
-			System.gc();
-		});
+        Thread t = new Thread(() -> {
+            for (File[] directory : files) {
+                if (cancel) {
+                    break;
+                }
+                importDirectory(directory);
+            }
 
-		t.start();
-	}
+            importDialog.closeDialogWithDelay();
+            errorHandler.showErrors();
+            modelHandler.updateTableUI();
+            System.gc();
+        });
 
-	private void setProgressBarLimits() {
-		int foldersCount = files.size();
-		int[] filesInFolders = new int[foldersCount];
-		int totalFiles = 0;
+        t.start();
+    }
 
-		for (int i = 0; i < foldersCount; i++) {
-			filesInFolders[i] = files.get(i).length;
-			totalFiles += filesInFolders[i];
-		}
+    private void setProgressBarLimits() {
+        int foldersCount = files.size();
+        int[] filesInFolders = new int[foldersCount];
+        int totalFiles = 0;
 
-		importDialog.setFileCount(totalFiles);
-		importDialog.setFoldersCount(foldersCount);
-		importDialog.setFilesInFolderCount(filesInFolders);
-	}
+        for (int i = 0; i < foldersCount; i++) {
+            filesInFolders[i] = files.get(i).length;
+            totalFiles += filesInFolders[i];
+        }
 
-	private void showProgressDialog() {
-		importDialog.setVisible(true);
-	}
+        importDialog.setFileCount(totalFiles);
+        importDialog.setFoldersCount(foldersCount);
+        importDialog.setFilesInFolderCount(filesInFolders);
+    }
 
-	private void importDirectory(File[] directory) {
-		for (File file : directory) {
-			if (cancel) {
-				break;
-			}
-			importDialog.updateCurrentFile(FilenameUtils.normalize(file.getPath()));
-			importFile(file);
-			importDialog.updateProgress();
-		}
-	}
+    private void showProgressDialog() {
+        importDialog.setVisible(true);
+    }
 
-	private void importFile(File file) {
-		// System.out.println("in import file:"+file);
-		try {
-			FileTreeTableRow.SubType subType = SupportedFileTypes.isPDF(file.getAbsolutePath())
-					? FileTreeTableRow.SubType.PDF
-							: FileTreeTableRow.SubType.IMAGE;
+    private void importDirectory(File[] directory) {
+        for (File file : directory) {
+            if (cancel) {
+                break;
+            }
+            importDialog.updateCurrentFile(FilenameUtils.normalize(file.getPath()));
+            importFile(file);
+            importDialog.updateProgress();
+        }
+    }
 
-			FileNodeFactory fileNodeFactory = NodeFactory.getFileNodeFactory(TreeTableRowType.REAL_FILE, subType);
-			if (fileNodeFactory instanceof RealPdfNodeFactory) {
-				((RealPdfNodeFactory) fileNodeFactory).setAutoRestrictionsNew(autoRestrioctionsNew);
-				((RealPdfNodeFactory) fileNodeFactory).setAutoRestrictionsOverwrite(autoRestrictionsOverwrite);
-			}
-			fileNodeFactory.setOptimize(optimizePDF);
-			fileNodeFactory.addPageEventListener(new PageEventListener() {
-				
-				public void pageCountChanged(int pages) {
-					importDialog.setPageCount(pages);
-				}
+    private void importFile(File file) {
+        // System.out.println("in import file:"+file);
+        try {
+            FileTreeTableRow.SubType subType = SupportedFileTypes.isPDF(file.getAbsolutePath())
+                ? FileTreeTableRow.SubType.PDF
+                : FileTreeTableRow.SubType.IMAGE;
 
-				
-				public void nextPage(int page) {
-					importDialog.updatePageProgress();
-				}
-			});
-			Node node = fileNodeFactory.getFileNode(file.getAbsolutePath());
-			if (node != null) {
-				modelHandler.insertFileNode(node);
-			}
-		} catch (Exception ex) {
-			Logger.getLogger(FileImporter.class.getName()).log(Level.SEVERE, null, ex);
-			String exceptionTrace = getExceptionTrace(ex);
-			errorHandler.reportError(file.getPath(), exceptionTrace);
-		}
-	}
+            FileNodeFactory fileNodeFactory = NodeFactory.getFileNodeFactory(TreeTableRowType.REAL_FILE, subType);
+            if (fileNodeFactory instanceof RealPdfNodeFactory) {
+                ((RealPdfNodeFactory) fileNodeFactory).setAutoRestrictionsNew(autoRestrioctionsNew);
+                ((RealPdfNodeFactory) fileNodeFactory).setAutoRestrictionsOverwrite(autoRestrictionsOverwrite);
+            }
+            fileNodeFactory.setOptimize(optimizePDF);
+            fileNodeFactory.addPageEventListener(new PageEventListener() {
 
-	private String getExceptionTrace(Exception ex) {
-		StringWriter sw = new StringWriter();
-		PrintWriter pw = new PrintWriter(sw);
-		ex.printStackTrace(pw);
+                public void pageCountChanged(int pages) {
+                    importDialog.setPageCount(pages);
+                }
 
-		return sw.toString();
-	}
 
-	public void setReadPageSize(boolean readPageSizeSelected) {
-		// TODO Auto-generated method stub
+                public void nextPage(int page) {
+                    importDialog.updatePageProgress();
+                }
+            });
+            Node node = fileNodeFactory.getFileNode(file.getAbsolutePath());
+            if (node != null) {
+                modelHandler.insertFileNode(node);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(FileImporter.class.getName()).log(Level.SEVERE, null, ex);
+            String exceptionTrace = getExceptionTrace(ex);
+            errorHandler.reportError(file.getPath(), exceptionTrace);
+        }
+    }
 
-	}
+    private String getExceptionTrace(Exception ex) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        ex.printStackTrace(pw);
+
+        return sw.toString();
+    }
+
+    public void setReadPageSize(boolean readPageSizeSelected) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
